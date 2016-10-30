21f38d8ad30106cb67f6e08fe00933d61b91b593
AOA <akixe.otegi@sgsmap.com>
2016-10-30 11:35:36 +0100

    desabilito codigo ejemplo

--- tables/Editores.js
+++ tables/Editores.js
@@ -1,8 +1,13 @@
-var azureMobileApps = require('azure-mobile-apps');
-var table = azureMobileApps.table();
+// var azureMobileApps = require('azure-mobile-apps');
+// var table = azureMobileApps.table();
 
-table.columns = {
-    "name" : "string",
-    "secondName" : "string"
-}
-module.exports = table;
+// table.columns = {
+//     "name" : "string",
+//     "secondName" : "string"
+// }
+
+// table.read.access = 'anonymous';
+// table.update.access = 'disable';
+
+
+// module.exports = table;
